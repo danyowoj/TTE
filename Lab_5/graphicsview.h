d0323f09caaf43c2a06a421dbc01e3843529f399
Alvarise <Alvarise@DESKTOP-B3ODSGQ>
2024-11-20 15:42:26 +0300

    Eraser function

--- Lab_5/graphicsview.h
+++ Lab_5/graphicsview.h
@@ -10,6 +10,7 @@
 #include <QScrollBar>
 #include <QGraphicsItem>
 
+
 class GraphicsView : public QGraphicsView
 {
     Q_OBJECT
@@ -18,8 +19,9 @@ public:
     explicit GraphicsView(QGraphicsScene *scene, QWidget *parent = nullptr);
     ~GraphicsView() override;
     void setPen(const QPen &pen);
+    void setEraserMode(bool mode);
 
-    signals:
+signals:
     void resized();
     void viewportChanged();
 
@@ -33,14 +35,16 @@ protected:
             emit resized(); // Испускаем сигнал при каждом изменении размера
         }
     void scrollContentsBy(int dx, int dy) override;
+    bool isWithinBounds(QGraphicsItem* item, QPointF newPos);
 
 private:
-    QPoint lastPoint;  // Текущая точка рисования
-    QColor currentColor;  // Цвет кисти
-    bool isDrawing;  // Флаг, рисуем ли мы
+    QPoint lastPoint;    // Текущая точка рисования
+    QColor currentColor; // Цвет кисти
+    bool isDrawing;      // Флаг, рисуем ли мы
     bool isMovingShape;
     QPen currentPen;
-
+    QPoint lastMousePos;
+    bool isEraserMode = false;
 };
 
 #endif // GRAPHICSVIEW_H
